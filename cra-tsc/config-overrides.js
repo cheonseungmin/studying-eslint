10d3e5bc1383ac2e58a25392177f308836bdb0a7
천승민 <cheon.sm@gsretail.com>
2021-12-28 16:11:27 +0900

    add webpack alias @src

---
+++ cra-tsc/config-overrides.js
@@ -0,0 +1,11 @@
+const {
+  override,
+  addWebpackAlias,
+} = require('customize-cra');
+const path = require('path');
+
+module.exports = override(
+  addWebpackAlias({
+    '@src': path.resolve(__dirname, 'src'),
+  }),
+);
